f75440bd343a92ff6d706742f0e04dd7bab19388
aui <g-keardkeawfa@rozetta.jp>
2021-03-17 22:07:02 +0900

    updated consonant unicode

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ th-brailler-js
 
 A JavaScript snippet to convert Thai characters into brailler alphabet.
 
-Load index.html into your HTML5 browser to see a simple demo.
+Load index.html into your browser to see a simple demo.
 
 License: MIT
--- th-brailler.js
+++ th-brailler.js
@@ -9,16 +9,53 @@
 "ล","ว","ศ","ษ","ส",
 "ห","ฬ","อ","ฮ"
 */
-const lookupMap = {};
+const lookupMap = {' ': "<br/>"};
 
 const th_consonant_braille = {
-  ก:'\u281b',
-  ข:'\u2805',
-  ฃ:'\u2834'+'\u2805',
-  ค:'\u2805',
-  ฅ:'\u2824'+'\u2805',
-  ฆ:'\u2820'+'\u2805',
-  ง:'\u283b',
+  ก: '\u281b',
+  ข: '\u2805',
+  ฃ: '\u2834'+'\u2805',
+  ค: '\u2805',
+  ฅ: '\u2824'+'\u2805',
+  ฆ: '\u2820'+'\u2805',
+  ง: '\u283b',
+  จ: '\u281a',
+  ฉ: '\u280c',
+  ช: '\u282c',
+  ซ: '\u282e',
+  ฌ: '\u2820'+'\u282c',
+  ญ: '\u2820'+'\u283d',
+  ฎ: '\u2820'+'\u2819',
+  ฏ: '\u2820'+'\u2833',
+  ฐ: '\u2820'+'\u281e',
+  ฑ: '\u2820'+'\u283e',
+  ฒ: '\u2824'+'\u283e',
+  ณ: '\u2820'+'\u281d',
+  ด: '\u2819',
+  ต: '\u2833',
+  ถ: '\u281e',
+  ท: '\u283e',
+  ธ: '\u2834'+'\u283e',
+  น: '\u281d',
+  บ: '\u2827',
+  ป: '\u282f',
+  ผ: '\u280f',
+  ฝ: '\u282d',
+  พ: '\u2839',
+  ฟ: '\u282b',
+  ภ: '\u2820'+'\u2839',
+  ม: '\u280d',
+  ย: '\u283d',
+  ร: '\u2817',
+  ล: '\u2807',
+  ว: '\u283a',
+  ศ: '\u2820'+'\u280e',
+  ษ: '\u2824'+'\u280e',
+  ส: '\u280e',
+  ห: '\u2813',
+  ฬ: '\u2820'+'\u2807',
+  อ: '\u2815',
+  ฮ: '\u283f',
 };
 
 /*
@@ -33,18 +70,21 @@ const th_consonant_braille = {
 */
 
 const th_vowel_braille = {
-
+  ะ:'a',
 };
 
 /*
-่" ่"," ้"," ๊"," ๋"
+่" ่"," ้"," ๊", ๋","ั"
 */
 
-const th_tone_braille = {
-  " ่":"",
-  " ้":"",
-  " ๊":"",
-  " ๋":""
+const th_top_braille = {
+  '่': "A",
+  '้': "B",
+  '๊': "C",
+  '๋': "D",
+  'ั': "E",
+  '์': "F",
+  '็': "G",
 };
 
 /*
@@ -53,16 +93,16 @@ const th_tone_braille = {
 */
 
 const number_brailler = {
-  "๑":"",
-  "๒":"",
-  "๓":"",
-  "๔":"",
-  "๕":"",
-  "๖":"",
-  "๗":"",
-  "๘":"",
-  "๙":"",
-  "๐":"",
+  '๑': '1',
+  '๒': '2',
+  '๓': '3',
+  '๔': '4',
+  '๕': '5',
+  '๖': '6',
+  '๗': '7',
+  '๘': '8',
+  '๙': '9',
+  '๐': '0',
 };
 
 function th_braillate(input) {
@@ -75,8 +115,8 @@ function th_braillate(input) {
         lookupMap[currentChar] = th_consonant_braille[currentChar];
       } else if (currentChar in th_vowel_braille) {
         lookupMap[currentChar] = th_vowel_braille[currentChar];
-      } else if (currentChar in th_tone_braille) {
-        lookupMap[currentChar] = th_tone_braille[currentChar];
+      } else if (currentChar in th_top_braille) {
+        lookupMap[currentChar] = th_top_braille[currentChar];
       } else {
         lookupMap[currentChar] = "?";
       }
